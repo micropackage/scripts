12f9aeff2df6095f3ff11d2cb01ac98dda7a3798
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-03-09 12:40:39 +0100

    update stylelint config

--- stylelint-config.js
+++ stylelint-config.js
@@ -5,6 +5,7 @@ module.exports = {
 	],
 	plugins: ['stylelint-order'],
 	rules: {
+		'function-no-unknown': null, // This rule doesn't work well with sass functions.
 		'order/order': ['custom-properties', 'declarations', 'rules'],
 		'order/properties-alphabetical-order': true,
 	},
