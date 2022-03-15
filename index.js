80de52df9e8184d8e86a88ceab17a4d19de5d391
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-03-15 09:20:31 +0100

    add eslint config to module exports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /**
  * Internal dependencies
  */
+const eslintConfig = require('./eslint-config');
 const RemoveSuprefluousAssetsPlugin = require('./plugins/remove-superfluous-assets');
 const webpackConfig = require('./config/webpack.config');
 
 module.exports = {
+	eslintConfig,
 	RemoveSuprefluousAssetsPlugin,
 	webpackConfig,
 };
